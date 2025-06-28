f16b0d55dbd087331bfb235990d3fa05e5980ff3
zmthall <74847979+zmthall@users.noreply.github.com>
2025-06-27 20:40:23 -0600

    Setup layouts for nuxt

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: "DefaultPages"
+})
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Home Page
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: "HomePage"
+})
+
+</script>
+
+<style>
+
+</style>
